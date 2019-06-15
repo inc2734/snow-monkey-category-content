6c51003b531b156d1eda5ee1112e9fe4908c19e5
Takashi Kitajima <inc@2inc.org>
2019-06-15 17:08:11 +0900

    0.0.2

--- snow-monkey-category-content.php
+++ snow-monkey-category-content.php
@@ -2,7 +2,7 @@
 /**
  * Plugin name: Snow Monkey Category Content
  * Description: Require Snow Monkey v7 or more
- * Version: 0.0.1
+ * Version: 0.0.2
  * Author: inc2734
  * Author URI: https://2inc.org
  * License: GPL2 or later
